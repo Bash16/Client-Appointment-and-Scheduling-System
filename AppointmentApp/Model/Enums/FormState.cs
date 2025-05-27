f0e5a1b5a8257358d1155827bf0ff9f18e7506d3
Mats100 <Muneeb75@icloud.com>
2025-05-27 14:59:57 +0500

    Files committed

---
+++ AppointmentApp/Model/Enums/FormState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentApp.Model.Enums
+{
+    public enum FormState
+    {
+        Customers,
+        Appointments,
+        Reports,
+        TypePerMonth,
+        Consultants,
+        CountPerLocation,
+    }
+}
